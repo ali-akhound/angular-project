94e30ae55ca8d666f30ba8c11db3f281aa1ea177
Ali Akhound <119714990+ali-akhound@users.noreply.github.com>
2025-02-09 22:18:13 -0500

    Update course resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogeComponent } from './cataloge/cataloge.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogeComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
