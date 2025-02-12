c1c3c1b846ad814a9e25f41686e4b5f51315a8a3
Ali Akhound <ali.akhound@outlook.com>
2025-02-12 17:54:33 -0500

    added http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogeComponent } from './cataloge/cataloge.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CatalogeComponent,
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
